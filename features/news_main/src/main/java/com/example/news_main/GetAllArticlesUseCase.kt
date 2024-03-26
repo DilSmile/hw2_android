aaf5ab48c7b841ac6600f6941ef24b379b49ded6
Admin <210103077@stu.sdu.edu.kz>
2024-03-26 16:45:13 +0600

    News App

--- features/news_main/src/main/java/com/example/news_main/GetAllArticlesUseCase.kt
+++ features/news_main/src/main/java/com/example/news_main/GetAllArticlesUseCase.kt
@@ -1,12 +1,13 @@
 package com.example.news_main
 
 import com.example.news_data.ArticlesRepository
+import com.example.news_data.RequestResult
 import com.example.news_data.model.Article
 import kotlinx.coroutines.flow.Flow
 
 class GetAllArticlesUseCase(private val repository:ArticlesRepository) {
 
-    operator fun invoke(): Flow<Article> {
+    operator fun invoke(): RequestResult<Flow<List<Article>>> {
         return repository.getAll()
     }
 }
