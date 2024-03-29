ca8952556ff009fc8ba2a6d4b4bcac8352c03797
Admin <210103077@stu.sdu.edu.kz>
2024-03-30 04:12:02 +0600

    News App common utils

--- features/news_main/src/main/java/com/example/news_main/GetAllArticlesUseCase.kt
+++ features/news_main/src/main/java/com/example/news_main/GetAllArticlesUseCase.kt
@@ -3,11 +3,14 @@ package com.example.news_main
 import com.example.news_data.ArticlesRepository
 import com.example.news_data.RequestResult
 import com.example.news_data.map
+import jakarta.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import com.example.news_data.model.Article as DataArticle
 
-class GetAllArticlesUseCase(private val repository: ArticlesRepository) {
+class GetAllArticlesUseCase @Inject constructor(
+    private val repository: ArticlesRepository)
+{
     operator fun invoke(): Flow<RequestResult<List<Article>>> {
        return repository.getAll()
             .map{ requestResult ->
---
+++ news-common/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.news.common"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+}
