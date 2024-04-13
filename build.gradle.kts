ea112f37c55c52fe62a08a271d7ef3eda9005ceb
Admin <210103077@stu.sdu.edu.kz>
2024-04-13 13:26:22 +0500

    News App generate Detect

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0" apply false
@@ -8,4 +10,19 @@ plugins {
     id("com.android.library") version "8.2.0" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
+    id("io.gitlab.arturbosch.detekt") version("1.23.3") apply false
+}
+
+allprojects.onEach {project ->
+    with(project.plugins){
+        if (hasPlugin("org.jetbrains.kotlin.android".get().pluginId)
+            ||hasPlugin("org.jetbrains.kotlin.android".get().pluginId)
+        ){
+            apply("io.gitlab.arturbosch.detekt".get().pluginId)
+            project.extensions.configure<DetektExtension>{
+                config = rootProject.files("default-detekt-config.yml")
+            }
+            project.dependencies.add("detektPlugins","io.gitlab.arturbosch.detekt:detekt-formatting".get().toString())
+        }
+    }
 }
