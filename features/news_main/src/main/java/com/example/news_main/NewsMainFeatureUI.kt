c0b0b2f0caa4d5bfc443b7740fd66a5ab8c5cee4
Admin <210103077@stu.sdu.edu.kz>
2024-04-13 11:22:08 +0500

    News App добавление картинок через Coil 2

--- features/news_main/src/main/java/com/example/news_main/NewsMainFeatureUI.kt
+++ features/news_main/src/main/java/com/example/news_main/NewsMainFeatureUI.kt
@@ -6,8 +6,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -19,6 +22,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.key
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.ModifierLocalReadScope
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -88,7 +93,7 @@ private fun ProgressIndicator(state: State.Loading){
 private fun Articles(
     @PreviewParameter(ArticlesPreviewProvider::class, limit = 1)articles: List<ArticleUI>,
 ){
-    LazyColumn {
+    LazyColumn() {
         items(articles){article ->
             key(article.id){
                 Article(article = article)
@@ -106,10 +111,14 @@ private fun Articles(
 internal fun Article(
     @PreviewParameter(ArticlePreviewProvider::class, limit = 1)article: ArticleUI,
 ) {
-   Row{
+   Row(Modifier.padding(bottom = 4.dp)){
        article.imageUrl?.let{imageUrl->
-           AsyncImage(model = imageUrl,contentDescription = stringResource(R.string.cotent_article_image))
-
+           AsyncImage(
+               model = imageUrl,
+               contentDescription = stringResource(R.string.cotent_article_image),
+               contentScale = ContentScale.Crop,
+               modifier = Modifier.heightIn(150.dp).widthIn(max=150.dp)
+           )
        }
        Column(modifier = Modifier.padding(8.dp)){
            Text(
