1d72056e73c472f63bedf5db078333adebaf3c99
Admin <210103077@stu.sdu.edu.kz>
2024-04-13 11:37:20 +0500

    News App работа с непрогрузкой картинки

--- features/news_main/src/main/java/com/example/news_main/NewsMainFeatureUI.kt
+++ features/news_main/src/main/java/com/example/news_main/NewsMainFeatureUI.kt
@@ -19,7 +19,11 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -30,6 +34,7 @@ import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
 import com.example.news.uikit.NewsTheme
 
 @Composable
@@ -113,12 +118,20 @@ internal fun Article(
 ) {
    Row(Modifier.padding(bottom = 4.dp)){
        article.imageUrl?.let{imageUrl->
-           AsyncImage(
-               model = imageUrl,
-               contentDescription = stringResource(R.string.cotent_article_image),
-               contentScale = ContentScale.Crop,
-               modifier = Modifier.heightIn(150.dp).widthIn(max=150.dp)
-           )
+           var isImageVisible by remember{ mutableStateOf(true) }
+           if(isImageVisible){
+               AsyncImage(
+                   model = imageUrl,
+                   onState = {state ->
+                             if (state is AsyncImagePainter.State.Error){
+                                 isImageVisible = false
+                             }
+                   },
+                   contentDescription = stringResource(R.string.cotent_article_image),
+                   contentScale = ContentScale.Crop,
+                   modifier = Modifier.size(150.dp)
+               )
+           }
        }
        Column(modifier = Modifier.padding(8.dp)){
            Text(
